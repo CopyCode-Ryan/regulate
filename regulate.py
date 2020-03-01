2dee83c9d6b5524a5d7822a5fe0f14cbc52d4f23
Ryan <ruifafan@gmail.com>
2020-03-01 15:24:58 +0800

    修改爬虫处理

--- regulate.py
+++ regulate.py
@@ -1,149 +1,129 @@
+# -*- coding: UTF-8 -*-
+
 import requests
 from lxml import etree
-import json
-import fcntl
-import hashlib
-import os
 import threading
+import json
+import time
 
-class GraduateSpider:
-    def __init__(self):
-        self.provinceName = 'province.txt'
-        self.provinceUrl = 'https://souky.eol.cn/api/newapi/func_11kytj'
-        self.url = "https://souky.eol.cn/api/newapi/func_11kytj?province={}&flag={}&tj_speciality={}&Mypage={}"
-        self.headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"}
-        self.province_list = self.provinces()
-        pass
+
+class RegulateSpider:
+
+    regulates = list()
+
+    def __init__(self, provinces, codes, url=None, headers=None):
+        if url is not None:
+            self.url = url
+        else:
+            self.url = "https://souky.eol.cn/api/newapi/func_11kytj"
+        if headers is not None:
+            self.headers = headers
+        else:
+            self.headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"}
+        self.codes = codes
+        self.provinces = provinces
 
     def parseUrl(self, url):
         response = requests.get(url, headers=self.headers)
         return response.content.decode()
 
     def run(self):
-        for province in self.province_list:
+        for i in range(len(self.provinces)):
+            province = self.provinces[i]
+            code = self.codes[i]
             if province != '':
-                print(province, "start::")
-                t = threading.Thread(target=self.getList, args=(province,))
+                t = threading.Thread(target=self.spider, args=(province, code,))
+                print(province, "start:")
                 t.start()
-                #process = multiprocessing.Process(target=self.getList, args=(province,))
-                #process.start()
-    
-    def getList(self, province):
-        next_url = self.url.format(province,'','','')
+                time.sleep(0.5)
+
+    def spider(self, province, code):
+        nextUrl = self.url + "?province=" + province
         items = list()
-        while next_url is not None:
-            content = self.parseUrl(next_url)
-            #print(content)
-            html = etree.HTML(content)
-            pages = html.xpath("//div[@class='page']")
-            self.parseContent(content)
-            item = self.parseContent(content)
-            for page in pages:
-                next_url = self.provinceUrl + page.xpath("//a[@class='pageBtn next']/@href")[0] if len(page.xpath("//a[@class='pageBtn next']/@href")) > 0 else None
-                if next_url is not None:
-                    break
+        while nextUrl is not None:
+            content = self.parseUrl(nextUrl)
+            item = self.cleanData(content, province, code)
             if item is not None:
                 items.extend(item)
-        if len(items) > 0 :
-            self.cacheData(province, items)
-        print("%s end." % province)
-        
-    def cacheData(self, name,items):
-        m = hashlib.md5()
-        m.update(name.encode())
-        fileName = m.hexdigest() + '.txt'
-        with open(fileName, 'w') as f:
-            fcntl.flock(f, fcntl.LOCK_EX)
-            f.write(json.dumps(items, ensure_ascii=False, indent=2))
-            fcntl.flock(f, fcntl.LOCK_UN)
+            nextUrl = self.parseNext(content)
+        RegulateSpider.regulates.extend(items)
+        #print(RegulateSpider.regulates)
+        print(province, code, items)
 
-    def parseContent(self, content):
+    def cleanData(self, content, province, code):
         html = etree.HTML(content)
         table = html.xpath("//div[@class='table']")[0]
         trs = table.xpath("//tr[@class='dataRow']")
-        label = ["name", "title", "type", "detail"]
         items = list()
         for tr in trs:
             tds = tr.xpath("./td")
-            if len(tds) > 1: 
-                item = {"name": '', "title": '', "type": '', "detail": ''}
+            if len(tds) > 1:
+                item = {"area": province, "area_code": str(code), "school_name": '', "majors": '', "school_type": '',
+                        "school_detail": ''}
                 i = 0
                 for td in tds:
                     text = ''
                     if td.text == '暂无结果':
                         item = None
                         break
-                    if len(td.xpath("./a/@href"))>0:
+                    if len(td.xpath("./a/@href")) > 0:
                         text = td.xpath("./a/@href")[0]
                     else:
                         text = td.text
                     if i == 0:
-                        item['name'] = text
+                        item['school_name'] = text
                     elif i == 1:
-                        item['title'] = text
+                        item['majors'] = text
                     elif i == 2:
-                        item['type'] = text
+                        item['school_type'] = text
                     elif i == 3:
-                        item['detail'] = text
+                        item['school_detail'] = text
                     i += 1
                 if item is not None:
                     items.append(item)
         return items
 
-    def provinces(self):
-        if os.path.exists(self.provinceName):
-            with open(self.provinceName, 'r') as f:
-                content = f.read()
-                p = json.loads(content)
-                f.close()
-                return p
-        else:
-            provinceHtml = self.parseUrl(self.provinceUrl)
-            items = self.getProvince(provinceHtml)
-            self.saveProvince(items)
-            return items
+    def parseNext(self, content):
+        html = etree.HTML(content)
+        pages = html.xpath("//div[@class='page']")
+        for page in pages:
+            next_url = self.url + page.xpath("//a[@class='pageBtn next']/@href")[
+                0] if len(page.xpath("//a[@class='pageBtn next']/@href")) > 0 else None
+            if next_url is not None:
+                return next_url
+        return None
 
-    def getProvince(self, provinceHtml):
-        html = etree.HTML(provinceHtml)
-        province = html.xpath("//select[@name='province']")
-        item = {}
-        for p in province:
-            item = p.xpath("./option/@value")
-        return item
-        
-    def saveProvince(self, provinces): 
-        with open(self.provinceName, "w") as f:
-            f.write(json.dumps(provinces, ensure_ascii=False, indent=2))
 
-    def charts(self):
-        while True:
-            if threading.activeCount() == 1:
-                break
-        province = self.provinces()
-        jsons = list()
-        for p in province:
-            m = hashlib.md5()
-            m.update(p.encode())
-            fileName = m.hexdigest() + '.txt'
-            try:
-                 with open(fileName, 'r') as f:
-                    content = f.read()
-                    f.close()
-                    items =  json.loads(content)
-                    item = {"name": p, "value":len(items), "selected": 'true', "items": items}
-                    jsons.append(item)
-            except Exception as e:
-                pass
-        with open("data.js", 'w') as f:
-            fcntl.flock(f, fcntl.LOCK_EX)
-            f.write("var data = " + json.dumps(jsons, ensure_ascii=False, indent=2))
-            fcntl.flock(f, fcntl.LOCK_UN)
-            f.close()
-        pass
+province_list = [
+    "北京", "天津", "河北", "山西", "内蒙古", "辽宁", "吉林", "黑龙江", "上海", "江苏", "浙江", "安徽", "福建", "江西", "山东", "河南", "湖北", "湖南", "广东",
+    "广西", "海南", "重庆", "四川", "贵州", "云南", "西藏", "陕西", "甘肃", "青海", "宁夏", "新疆", "香港", "澳门"]
+province_code = [
+    110000, 120000, 130000, 140000, 150000, 210000, 220000, 230000, 310000, 320000, 330000, 340000, 350000, 360000,
+    370000,
+    410000, 420000, 430000, 440000, 450000, 460000, 500000, 510000, 520000, 530000, 540000, 610000, 620000, 630000,
+    640000,
+    650000, 810000, 820000]
+
+
+def update_data(url):
+    while True:
+        thread_num = len(threading.enumerate())
+        print(thread_num)
+        if thread_num <= 1:
+            break
+    if len(RegulateSpider.regulates) > 0:
+        header = {'Content-Type': 'application/json'}
+        response = requests.post(url, headers=header, data=json.dumps(RegulateSpider.regulates))
+        print(response.content.decode())
+
+
 def main():
-    spider = GraduateSpider()
+    spider = RegulateSpider(provinces=province_list, codes=province_code)
     spider.run()
-    spider.charts()
+    update_data("https://graduate.xiaoyelive.com/api/v1.0/district/school-majors")
+    pass
+
 
 if __name__ == "__main__":
     main()
